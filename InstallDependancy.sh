f0536efbe6be1da0277a8867f0759b0e6b9609cd
John <johnsoupir@gmail.com>
2021-11-03 19:34:50 -0500

    Added dependancies installer

---
+++ InstallDependancy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "++++++++++++++++++++++++++++++++++++++++"
+echo "installing dependancies for music system"
+echo "++++++++++++++++++++++++++++++++++++++++"
+echo ""
+echo "Installing packages: python3 python3-pip espeak youtube-dl pipx" 
+sudo apt install -y python3 python3-pip espeak youtube-dl pipx
+echo "Done." 
+echo "Installing yewtube"
+pip3 install pipx
+pipx install git+https://github.com/iamtalhaasghar/yewtube.git
+pipx inject yewtube youtube-dl
+echo "Done."
+echo "Installing Flask"
+pip3 install flask
+echo "Done."
+echo "Remember to set API key in yewtube"
+
+echo "++++++++++++++++++++++++++++++++++++++++"
